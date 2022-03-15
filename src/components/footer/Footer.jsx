3321483f8b86b3b1679c3a893f2242dfc81f477b
micael <mickii.gomez.mg@gmail.com>
2022-03-15 00:17:08 -0300

    another

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./footer.css"
+
+export const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="title">
+            
+            <h3><span>Weather</span> Web</h3>
+        </div>
+        
+    </div>
+  )
+}
